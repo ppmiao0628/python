800d26f3e539ce0a9bc3e93a1706c1933312d64e
mpp <ppmiao0628@outlook.com>
2017-10-09 21:44:05 +0800

    进程通信

---
+++ processAccessDemo.py
@@ -0,0 +1,34 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, time, random
+from multiprocessing import Process, Queue
+
+
+# 设置一个写入的进程
+def writePro(p):
+    print('process %s start to write...' % os.getpid())
+    for value in ['a', 'b', 'c', 'd']:
+        print('put %s to the queue' % value)
+        p.put(value)
+        time.sleep(random.random())
+    return
+
+
+# 设置一个读取的进程
+def readPro(p):
+    print('process %s start to read...' % os.getpid())
+    while (True):
+        value = p.get(True)
+        print('read %s from the queue' % value)
+    return
+
+
+if __name__ == '__main__':
+    q = Queue()
+    wp = Process(target=writePro, args=(q,))
+    rp = Process(target=readPro, args=(q,))
+    wp.start()
+    rp.start()
+    wp.join()
+    rp.terminate()
